81d448ab16ed1f7127bf21df03da5358919b898f
Dmitry Lernatovich <dlernatovich@geeksforless.net>
2024-11-20 22:34:18 +0100

    Initial commit.

---
+++ AnalyticsWorkspace/AnalyticsWorkspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsWorkspaceApp.swift
+//  AnalyticsWorkspace
+//
+//  Created by Dmitry Lernatovich on 20.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsWorkspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalyticsWorkspaceTests/AnalyticsWorkspaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsWorkspaceTests.swift
+//  AnalyticsWorkspaceTests
+//
+//  Created by Dmitry Lernatovich on 20.11.2024.
+//
+
+import Testing
+@testable import AnalyticsWorkspace
+
+struct AnalyticsWorkspaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
